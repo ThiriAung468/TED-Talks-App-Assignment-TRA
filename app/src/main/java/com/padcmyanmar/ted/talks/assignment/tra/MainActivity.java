e0cf314e8d73eb818d8054e13f85a95f2ad9449a
Thiri Aung <thiriaung468@gmail.com>
2018-06-02 07:57:47 +0630

    Input images TRA.

--- app/src/main/java/com/padcmyanmar/ted/talks/assignment/tra/MainActivity.java
+++ app/src/main/java/com/padcmyanmar/ted/talks/assignment/tra/MainActivity.java
@@ -4,10 +4,14 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.padcmyanmar.ted.talks.assignment.tra.adapters.TedAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +20,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        RecyclerView rvTed = findViewById(R.id.rv_ted_talks);
+        TedAdapter tedAdapter = new TedAdapter();
+        rvTed.setAdapter(tedAdapter);
+        rvTed.setLayoutManager(new LinearLayoutManager(getApplicationContext(),
+                LinearLayoutManager.VERTICAL,
+                false));
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
---
+++ app/src/main/java/com/padcmyanmar/ted/talks/assignment/tra/viewholders/TedViewHolder.java
@@ -0,0 +1,16 @@
+package com.padcmyanmar.ted.talks.assignment.tra.viewholders;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import com.padcmyanmar.ted.talks.assignment.tra.R;
+
+public class TedViewHolder extends RecyclerView.ViewHolder {
+    public TedViewHolder(View itemView) {
+        super(itemView);
+
+
+    }
+
+
+}
